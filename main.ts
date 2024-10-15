21d7266176b095a64fd9eaadfdf4b1d0287fccf2
andgate <the.andgate@gmail.com>
2024-10-15 04:44:08 -0700

    Most recent attempt to implement this

--- main.ts
+++ main.ts
@@ -1,134 +1,171 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { RangeSet, RangeSetBuilder } from "@codemirror/state";
+import {
+	Decoration,
+	DecorationSet,
+	EditorView,
+	PluginSpec,
+	PluginValue,
+	ViewPlugin,
+	ViewUpdate,
+	WidgetType,
+} from "@codemirror/view";
+import { App, Plugin, PluginSettingTab, request, Setting } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ObeliskSettings {
+	llamaPort: number;
+	llamaVersion: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: ObeliskSettings = {
+	llamaPort: 11434,
+	llamaVersion: "3.2",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+let currentSettings: ObeliskSettings = DEFAULT_SETTINGS;
+
+export default class ObeliskPlugin extends Plugin {
+	settings: ObeliskSettings;
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new ObeliskSettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		console.log("Registering editor extension...");
+		this.registerEditorExtension([completionViewPlugin]);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		console.log("Obelisk plugin loaded successfully");
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
+		currentSettings = this.settings;
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+		currentSettings = this.settings;
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ObeliskSettingTab extends PluginSettingTab {
+	plugin: ObeliskPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ObeliskPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("ollama port")
+			.setDesc("port number of the ollama server")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter port number")
+					.setValue(this.plugin.settings.llamaPort.toString())
+					.onChange(async (value) => {
+						this.plugin.settings.llamaPort = parseInt(value);
+						await this.plugin.saveSettings();
+					}),
+			);
+
+		new Setting(containerEl)
+			.setName("Llama Version")
+			.setDesc("Model version to use for text completion")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter model version")
+					.setValue(this.plugin.settings.llamaVersion)
+					.onChange(async (value) => {
+						this.plugin.settings.llamaVersion = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
+}
+
+export class EmojiWidget extends WidgetType {
+	toDOM(view: EditorView): HTMLElement {
+		const div = document.createElement("span");
+
+		div.innerText = "👉";
+
+		return div;
+	}
+}
+
+class CompletionViewPlugin implements PluginValue {
+	decorations: DecorationSet;
+
+	constructor(view: EditorView) {
+		this.decorations = this.buildDecorations(view);
+	}
+
+	update(update: ViewUpdate) {
+		if (update.docChanged || update.viewportChanged) {
+			this.decorations = this.buildDecorations(update.view);
+		}
+	}
+
+	destroy() {
+		// this.decorations.clear();
+	}
+
+	buildDecorations(view: EditorView): DecorationSet {
+		const builder = new RangeSetBuilder<Decoration>();
+
+		if (view.state.doc.length === 0) {
+			return RangeSet.empty;
+		}
+
+		const endPos = view.state.doc.length;
+		const decoration = Decoration.widget({
+			class: "completion-decoration",
+			widget: new EmojiWidget(),
+		});
+
+		builder.add(0, endPos, decoration);
+		return builder.finish();
+	}
+}
+
+const pluginSpec: PluginSpec<CompletionViewPlugin> = {
+	decorations: (value: CompletionViewPlugin) => value.decorations,
+};
+
+export const completionViewPlugin = ViewPlugin.fromClass(
+	CompletionViewPlugin,
+	pluginSpec,
+);
+
+async function fetchCompletion(query: string): Promise<string | null> {
+	console.log("Fetching completion for:", query);
+	const response = await request({
+		url: `http://localhost:${currentSettings.llamaPort}/api/generate`,
+		method: "POST",
+		body: JSON.stringify({
+			model: `llama${currentSettings.llamaVersion}`,
+			prompt: query,
+			stream: false,
+			options: {
+				temperature: 0.7,
+			},
+		}),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	const result = JSON.parse(response);
+	return result.response || null;
 }
